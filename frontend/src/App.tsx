080cbc26a82ae313d3d1de0c17b825c046fed864
Thomas Borck <coding@bommy.net>
2025-10-09 17:39:48 +0200

    frontend: added page navigation

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,10 +1,22 @@
-import { Route, Routes } from "react-router-dom";
+import { Link, Route, Routes } from "react-router-dom";
 import { TimebookPage } from "./pages/TimebookPage";
+import { DummyTestPage } from "./pages/DummyTestPage";
+
+import "./App.css";
 
 export function App() {
     return (
-        <Routes>
-            <Route path="/" element={<TimebookPage />} />
-        </Routes>
+        <>
+            <nav>
+                <Link to="/">Timebook</Link>
+                <Link to="/dummy">Dummy Test</Link>
+            </nav>
+            <main>
+                <Routes>
+                    <Route path="/" element={<TimebookPage />} />
+                    <Route path="/dummy" element={<DummyTestPage />} />
+                </Routes>
+            </main>
+        </>
     );
 }
